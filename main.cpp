e4569edd9f52fc54fa36751c74c6570d2eaef96a
PCA-NetRom <pca@netrom.ro>
2024-02-06 14:01:58 +0200

    Update solution with hands-on result from STL#2
    Include updated input file for STL#3

--- main.cpp
+++ main.cpp
@@ -3,53 +3,97 @@
 #include <string>
 #include <algorithm>
 #include <fstream>
+#include <queue>
+#include <map>
 
-using namespace std;
+static const int k_hospitalOpenTime = 9;
+static const int k_hospitalCloseTime = 17;
 
-struct Problem {
-    string name;
-    string speciality;
+struct Problem 
+{
+    std::string name;
+    std::string speciality;
+    int priority;
+    int duration;
+
+    bool operator< (const Problem& other) const 
+    {
+        if (priority != other.priority) 
+            return (priority < other.priority);
+
+        return (duration > other.duration);
+    }
 };
 
-struct Doctor {
-    string id;
-    string speciality;
+struct Doctor 
+{
+    std::string id;
+    std::string speciality;
+    int availableFromHour;
+    std::vector<Problem> solvedProblems;
 };
 
-Problem currentProblem;
+int main() 
+{
+    // Read input
+    std::ifstream inFile("inputSTL2.txt");
 
-bool matchesSpeciality(const Doctor &doctor) {
-    return doctor.speciality == currentProblem.speciality;
-}
+    int numProblems, numDoctors;
+    inFile >> numProblems;
 
-int main() {
-    ifstream inFile("HandsOn-Input.txt");
-    
-    int n, m;
-    inFile >> n;
-    
-    vector<Problem> problems(n);
-    for (Problem &p : problems) {
-        inFile >> p.name >> p.speciality;
+    std::vector<Problem> problems(numProblems);
+    std::priority_queue<Problem> problemsQueue;
+    for (Problem& problem : problems) 
+    {
+        inFile >> problem.name >> problem.speciality >> problem.duration >> problem.priority;
+        problemsQueue.push(problem);
     }
 
-    inFile >> m;
-    vector<Doctor> doctors(m);
-    for (Doctor &d : doctors) {
-        inFile >> d.id >> d.speciality;
-    }
-
-    for (const Problem &p : problems) {
-        currentProblem = p;
-        auto it = find_if(doctors.begin(), doctors.end(), matchesSpeciality);
-
-        if (it != doctors.end()) {
-            cout << it->id << " " << p.name << endl;
-            doctors.erase(it);
-        }
+    inFile >> numDoctors;
+    std::vector<Doctor> doctors(numDoctors);
+    for (Doctor& doctor : doctors) 
+    {
+        inFile >> doctor.id >> doctor.speciality;
+        doctor.availableFromHour = k_hospitalOpenTime;
     }
 
     inFile.close();
-    
+
+    // Solve
+	while (!problemsQueue.empty()) 
+    {
+		Problem currentProblem = problemsQueue.top();
+		problemsQueue.pop();
+		std::vector<Doctor>::iterator foundDoctor = find_if(doctors.begin(), doctors.end(), [=](const Doctor& doctor) 
+        {
+            if (doctor.availableFromHour + currentProblem.duration <= k_hospitalCloseTime
+                && doctor.speciality == currentProblem.speciality)
+            {
+                return true;
+            }
+
+			return false;
+		});
+
+		if (foundDoctor != doctors.end()) 
+        {
+			foundDoctor->availableFromHour += currentProblem.duration;
+			foundDoctor->solvedProblems.push_back(currentProblem);
+		}
+	}
+
+    // Print output
+    for (const Doctor& doctor : doctors) 
+    {
+        if (doctor.solvedProblems.size() > 0) 
+        {
+            std::cout << doctor.id << " " << doctor.solvedProblems.size() << " ";
+            for (const Problem& problem : doctor.solvedProblems) 
+                std::cout << problem.name << " ";
+
+            std::cout << std::endl;
+        }
+    }
+
     return 0;
 }
