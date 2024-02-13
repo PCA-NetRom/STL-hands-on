61eaa304ebdab245bfc1860b671f0aa28d70da7e
PCA-NetRom <pca@netrom.ro>
2024-02-13 15:36:21 +0200

    Update solution with hands-on result from STL3#

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <fstream>
 #include <queue>
 #include <map>
+#include <set>
 
 static const int k_hospitalOpenTime = 9;
 static const int k_hospitalCloseTime = 17;
@@ -13,11 +14,16 @@ struct Problem
 {
     std::string name;
     std::string speciality;
-    int priority;
+    int arrivalHour;
     int duration;
+    int priority;
 
     bool operator< (const Problem& other) const 
     {
+        // Order by arrival hour, then priority, then duration
+        if (arrivalHour != other.arrivalHour)
+            return (arrivalHour > other.arrivalHour);
+
         if (priority != other.priority) 
             return (priority < other.priority);
 
@@ -28,7 +34,8 @@ struct Problem
 struct Doctor 
 {
     std::string id;
-    std::string speciality;
+    std::set<std::string> specialities;
+
     int availableFromHour;
     std::vector<Problem> solvedProblems;
 };
@@ -36,25 +43,45 @@ struct Doctor
 int main() 
 {
     // Read input
-    std::ifstream inFile("inputSTL2.txt");
+    std::ifstream inFile("HandsOn-Input.txt");
 
-    int numProblems, numDoctors;
+    int numProblems;
     inFile >> numProblems;
 
-    std::vector<Problem> problems(numProblems);
     std::priority_queue<Problem> problemsQueue;
-    for (Problem& problem : problems) 
+    for (int problemIdx = 0; problemIdx < numProblems; problemIdx++)
     {
-        inFile >> problem.name >> problem.speciality >> problem.duration >> problem.priority;
+        Problem problem;
+        inFile >> problem.name >> problem.speciality >> problem.arrivalHour
+                >> problem.duration >> problem.priority;
         problemsQueue.push(problem);
     }
 
+    int numDoctors;
     inFile >> numDoctors;
-    std::vector<Doctor> doctors(numDoctors);
-    for (Doctor& doctor : doctors) 
+
+    std::map<std::string, Doctor> doctorsById;
+    std::map<std::string, std::set<std::string>> doctorIdsBySpeciality;
+    for (int doctorIdx = 0; doctorIdx < numDoctors; doctorIdx++)
     {
-        inFile >> doctor.id >> doctor.speciality;
+        Doctor doctor;
+        inFile >> doctor.id;
+        
+        int numSpecialities;
+        inFile >> numSpecialities;
+
+        for (int idx = 0; idx < numSpecialities; idx++)
+        {
+            std::string speciality;
+            inFile >> speciality;
+            doctor.specialities.insert(speciality);
+
+            doctorIdsBySpeciality[speciality].insert(doctor.id);
+        }
+
         doctor.availableFromHour = k_hospitalOpenTime;
+
+        doctorsById[doctor.id] = doctor;
     }
 
     inFile.close();
@@ -64,36 +91,43 @@ int main()
     {
 		Problem currentProblem = problemsQueue.top();
 		problemsQueue.pop();
-		std::vector<Doctor>::iterator foundDoctor = find_if(doctors.begin(), doctors.end(), [=](const Doctor& doctor) 
+
+        // Find available doctor
+        const auto& doctorsIt = doctorIdsBySpeciality.find(currentProblem.speciality);
+        if (doctorsIt == doctorIdsBySpeciality.end())
+            continue;       // No doctor for this speciality
+
+        const std::set<std::string>& doctorIdsSet = doctorsIt->second;
+        for (const std::string& doctorId : doctorIdsSet)
         {
-            if (doctor.availableFromHour + currentProblem.duration <= k_hospitalCloseTime
-                && doctor.speciality == currentProblem.speciality)
+            Doctor& doctor = doctorsById[doctorId];
+
+            if (doctor.availableFromHour <= currentProblem.arrivalHour
+                && currentProblem.arrivalHour + currentProblem.duration <= k_hospitalCloseTime)
             {
-                return true;
+                // Assign problem to doctor
+                doctor.availableFromHour = currentProblem.arrivalHour + currentProblem.duration;
+                doctor.solvedProblems.push_back(currentProblem);
+                break;
             }
-
-			return false;
-		});
-
-		if (foundDoctor != doctors.end()) 
-        {
-			foundDoctor->availableFromHour += currentProblem.duration;
-			foundDoctor->solvedProblems.push_back(currentProblem);
-		}
+        }
 	}
 
     // Print output
-    for (const Doctor& doctor : doctors) 
+    for (auto& doctorEntry : doctorsById)
     {
+        Doctor& doctor = doctorEntry.second;
         if (doctor.solvedProblems.size() > 0) 
         {
             std::cout << doctor.id << " " << doctor.solvedProblems.size() << " ";
             for (const Problem& problem : doctor.solvedProblems) 
-                std::cout << problem.name << " ";
+                std::cout << problem.name << " " << problem.arrivalHour << " ";
 
             std::cout << std::endl;
         }
     }
+
+    std::getchar();
 
     return 0;
 }
